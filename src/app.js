7e17cea868c3489b819235256402c9b6b7561ebb
Aleix Iglesias Pascual <aiglesiaspa@uoc.edu>
2024-11-15 10:18:42 +0100

    Added javascript functionality

--- src/app.js
+++ src/app.js
@@ -1,2 +1,13 @@
+
 const name = 'world';
 console.log(`Hello ${name}`);
+
+const changeButton = document.getElementById('changeButton');
+
+function changeTitle() {
+    const elem = document.querySelector('h1');
+    elem.textContent = '¡Boo!';
+    elem.style.color = '#4c0d06';
+}
+
+changeButton.addEventListener('click', changeTitle);
